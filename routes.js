36f909eb4c5f07ca1664f2d954b91c539c0520ac
mjovit <mjovit@gmail.com>
2019-04-29 16:30:23 +0530

    rebuild

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const routes = require('next-routes')();
 
 routes
     .add('/campaigns/new', '/campaigns/new')
-    .add('/dashboard','/dashboard')
     .add('/campaigns/getcandidate', '/campaigns/getcandidate')
     .add('/campaigns/totalvotes', '/campaigns/total');
 
